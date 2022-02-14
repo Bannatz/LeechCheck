35a352ab4d89d4c8431070d4078884746af078d7
MIROowo <aaronbruser@gmail.com>
2022-02-14 22:47:36 +0100

    Proxychecker fertig + utils erweiterung und main implementierung

--- proxychecker.py
+++ proxychecker.py
@@ -1,11 +1,32 @@
 import requests, os, sys
+from utils import cprint, listRemoveNewLines, log
 
-ptypes = ["http", "https"]
+def save_working(working: list):
+    if os.path.isdir("proxies") is False:
+        os.mkdir("proxies")
+    with open("proxies/working.txt", "a") as f:
+        for proxy in working:
+            f.write(f"{proxy}\n")
+        f.close()
 
 def proxy_check(proxy_list: list):
+    proxy_list = listRemoveNewLines(proxy_list)
+    working = []
 
-    pdict = {}
+    l = len(proxy_list)
+    z = 1
 
     for proxy in proxy_list:
-        pdict.update()
-        
+        sys.stdout.write("\033[K")
+        cprint(f" Testing: {proxy}\t[{z}/{l}]\r", "green")
+        z += 1
+        p = {"https": proxy}
+        try:
+            r = requests.get(url="https://example.com", proxies=p, timeout=5)
+            log(f"[ProxyChecker]\n{}")
+            r.close()
+            working.append(proxy)
+        except Exception:
+            pass
+    
+    save_working(working)
