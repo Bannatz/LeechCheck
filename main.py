f4e7c453af0f36d5eeae5376d1f5829465aaca9b
Bannatz <kevintoptschij12012003@gmail.com>
2022-04-14 20:19:15 +0200

    Updated Leecher, Added SearchResults.CC Leeching, Fixed Leeching

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup as soup
-import requests, sys
+import requests, sys, cloudscraper
 import re as reg
 from proxychecker import proxycheck
 from utils import cprint, savename, log, delDup
@@ -9,9 +9,8 @@ from utils import cprint, savename, log, delDup
 3. die page mit einem HTML Parser absuchen nach dem Tag "a" und der Klasse "link"
 4. For loop für jeden href er findet einen request senden und den parsen nach "h6"/Paste Name
 5. For loop nach der "textarea"/Alles was in dem Paste drinne steht
-6. Öffne eine Datei schreibe die URL die gerade gelesen wird rein und den Inhalt der Seite!
+6. Öffne eine Datei schreibe die URL die gerade gelesen wird rein und den Inhalt der Seite
 """
-
 def proxy():
     cprint("[LC] Do you want to check the proxies?[y/N] ", "green")
     ans = input("")
@@ -33,31 +32,7 @@ def proxy():
         pass
 
 def main():
-    cprint("""
-
-  /$$$$$$  /$$$$$$  /$$$$$$          /$$                                     /$$                                
- /$$__  $$|_  $$_/ /$$__  $$        | $$                                    | $$                                
-| $$  \ $$  | $$  | $$  \ $$        | $$        /$$$$$$   /$$$$$$   /$$$$$$$| $$$$$$$   /$$$$$$   /$$$$$$       
-| $$$$$$$$  | $$  | $$  | $$ /$$$$$$| $$       /$$__  $$ /$$__  $$ /$$_____/| $$__  $$ /$$__  $$ /$$__  $$      
-| $$__  $$  | $$  | $$  | $$|______/| $$      | $$$$$$$$| $$$$$$$$| $$      | $$  \ $$| $$$$$$$$| $$  \__/      
-| $$  | $$  | $$  | $$  | $$        | $$      | $$_____/| $$_____/| $$      | $$  | $$| $$_____/| $$            
-| $$  | $$ /$$$$$$|  $$$$$$/        | $$$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$  | $$|  $$$$$$$| $$            
-|__/  |__/|______/ \______/         |________/ \_______/ \_______/ \_______/|__/  |__/ \_______/|__/            
-                                                                                                                
-                                                                                                                
-                                                                                                                
- /$$$$$$$                       /$$               /$$                 /$$                                /$$    
-| $$__  $$                     | $$              | $$                | $$                               | $$    
-| $$  \ $$ /$$$$$$   /$$$$$$$ /$$$$$$    /$$$$$$ | $$$$$$$  /$$   /$$| $$$$$$$     /$$$$$$$   /$$$$$$  /$$$$$$  
-| $$$$$$$/|____  $$ /$$_____/|_  $$_/   /$$__  $$| $$__  $$| $$  | $$| $$__  $$   | $$__  $$ /$$__  $$|_  $$_/  
-| $$____/  /$$$$$$$|  $$$$$$   | $$    | $$$$$$$$| $$  \ $$| $$  | $$| $$  \ $$   | $$  \ $$| $$$$$$$$  | $$    
-| $$      /$$__  $$ \____  $$  | $$ /$$| $$_____/| $$  | $$| $$  | $$| $$  | $$   | $$  | $$| $$_____/  | $$ /$$
-| $$     |  $$$$$$$ /$$$$$$$/  |  $$$$/|  $$$$$$$| $$  | $$|  $$$$$$/| $$$$$$$//$$| $$  | $$|  $$$$$$$  |  $$$$/
-|__/      \_______/|_______/    \___/   \_______/|__/  |__/ \______/ |_______/|__/|__/  |__/ \_______/   \___/  
-                                                                                                                
-                                                                                                                
-
-""", "green")
+    scraper = cloudscraper.create_scraper()
     s = int(input("How many sites (2 for one Site): "))
     log(init=True)
     string = "/"
@@ -70,27 +45,28 @@ def main():
         urls.append(pagelist) 
         for url in urls: # for loop für die URLs
             cprint(f"{url}\n\n", "blue")
-            page = requests.get(url)
+            page = scraper.get(url)
             jesus = soup(page.content, 'html.parser')
             for i in jesus.find_all('a', class_="link", href=True):
-                re = requests.get("https://pastehub.net/" + i.text)
+                re = scraper.get("https://pastehub.net/" + i.text)
                 jesus3 = soup(re.content, "html.parser")
                 pagename = jesus3.find('h6')
                 cprint(pagename.text + " | " + "https://pastehub.net/" + i.text + "\n", "green")
                 pagename = pagename.text.replace(string, "")
-                for f in jesus3.find_all('textarea'): 
+                for f in jesus3.find_all('textarea'):
                     check = reg.findall(r".{1,}[@].{1,}\.[a-z]{1,}[:].{1,}", str(f.text))
                     log(f"[URL: {url}]\n{check}")
                     pcheck = reg.findall(r"[0-9]{2,3}\.[0-9]{2,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{2,4}", str(f.text))
                     log(pcheck)
-                    ucheck = reg.findall(r"^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9:]){1,}[a-zA-Z0-9]:.{1,}$", str(f.text)) # [a-zA-Z0-9] a-z groß und klein {1,} einmal bis unendlich [:] Doppelpunkt, . für jeden Character(!,\,$), {1,} eins bis ins unendliche
-                    log(ucheck)
+                    #ucheck = reg.findall(r"^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9:]){1,}[a-zA-Z0-9]:.{1,}$", str(f.text)) # [a-zA-Z0-9] a-z groß und klein {1,} einmal bis unendlich [:] Doppelpunkt, . für jeden Character(!,\,$), {1,} eins bis ins unendliche
+#
+#log(ucheck)
                     cprint(f"Combos: {len(check)}\n", "green")
-                    cprint(f"User:Pass Combos: {len(ucheck)}\n", "green")
+                    #cprint(f"User:Pass Combos: {len(ucheck)}\n", "green")
                     cprint(f"Proxies: {len(pcheck)}\n\n", "green")
                     cz = len(check)
                     pz = len(pcheck)
-                    uz = len(ucheck)
+                    #uz = len(ucheck)
                     z = 0
                     for combo in check:
                         temp = str(combo).split(" ")
@@ -106,14 +82,14 @@ def main():
                         z += 1
                         if z == pz:
                             break
-                    for ucombo in ucheck:
-                        temp = str(ucombo).split(" ")
-                        sys.stdout.write(f"Appending Proxy: {ucombo}\r")
-                        sys.stdout.write("\033[K")
-                        usercombos.append(ucombo)
-                        z += 1
-                        if z == pz:
-                            break
+                    #for ucombo in ucheck:
+                    #    temp = str(ucombo).split(" ")
+                    #    sys.stdout.write(f"Appending Proxy: {ucombo}\r")
+                    #    sys.stdout.write("\033[K")
+                    #    usercombos.append(ucombo)
+                    #    z += 1
+                    #    if z == pz:
+                    #        break
 
     with open(savename("combos", "txt"), "a") as k:
         for combo in delDup(combos):
@@ -125,10 +101,94 @@ def main():
             proxy = str(proxy).replace("\r", "")
             k.write(f"{proxy}\n")
         k.close()
-    with open(savename("userpass", "txt"), "a") as k:
-        for userpw in delDup(usercombos):
-            userpw = str(userpw).replace("\r", "")
-            k.write(f"{userpw}\n")
+    #with open(savename("userpass", "txt"), "a") as k:
+    #    for userpw in delDup(usercombos):
+    #        userpw = str(userpw).replace("\r", "")
+    #        k.write(f"{userpw}\n")
 
-main()
+
+def search():
+    keyword = str(input("Enter Keyword(example: Minecraft Combos): "))    
+    scraper = cloudscraper.create_scraper()
+    i = 0
+    urls = []
+    combos = []
+    proxies = []
+    if keyword == "":
+        print("Please specify a keyword!")
+        keyword = str(input("Enter Keyword: "))
+    elif " " in keyword:
+        keyword.replace(" ", "+")
+    elif "proxy" or "Proxy" in keyword:
+        page = scraper.get("https://searchresults.cc/" + keyword)
+        jesus = soup(page.content, 'html.parser')
+        for i in jesus.find_all("a", href=True):
+            if i.text.startswith("https://pastehub.net") == True:
+                urls.append(i.text)
+        for url in urls:
+            page = scraper.get(url)
+            jesus = soup(page.content, 'html.parser')
+            cprint(f"{url}\n\n", "blue")
+            pagename = jesus.find("h6")
+            for f in jesus.find_all('textarea'):
+                check = reg.findall(r"[0-9]{2,3}\.[0-9]{2,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{2,4}", str(f.text))
+                for proxy in check:
+                    temp = str(proxy).split(" ")
+                    sys.stdout.write(f"Appending Proxies: {proxy}\r")
+                    sys.stdout.write("\033[K")
+                    proxies.append(str(proxy))
+        with open(savename(f"{keyword}", "txt"), "a") as f:
+            for proxy in delDup(proxies):
+                proxy = str(proxy).replace("\r", "")
+                f.write(f"{proxy}\n")
+            f.close()
+    page = scraper.get("https://searchresults.cc/" + keyword)
+    jesus = soup(page.content, 'html.parser')
+    for i in jesus.find_all("a", href=True):
+        if i.text.startswith("https://pastehub.net") == True:
+            urls.append(i.text)
+    for url in urls:
+        page = scraper.get(url)
+        jesus = soup(page.content, 'html.parser')
+        cprint(f"{url}\n\n", "blue")
+        pagename = jesus.find("h6")
+        for f in jesus.find_all('textarea'):
+            check = reg.findall(r".{1,}[@].{1,}\.[a-z]{1,}[:].{1,}", str(f.text))
+            for combo in check:
+                temp = str(combo).split(" ")
+                combo = temp[0]
+                sys.stdout.write(f"Appending Combo: {combo}\r")
+                sys.stdout.write("\033[K")
+                combos.append(str(combo))
+    with open(savename(f"{keyword}", "txt"), "a") as f:
+        for combo in delDup(combos):
+            combo = str(combo).replace("\r", "")
+            f.write(f"{combo}\n")
+        f.close()
+def menu():
+    cprint("""
+
+.____                        .__    _________ .__                   __                
+|    |    ____   ____   ____ |  |__ \_   ___ \|  |__   ____   ____ |  | __            
+|    |  _/ __ \_/ __ \_/ ___\|  |  \/    \  \/|  |  \_/ __ \_/ ___\|  |/ /            
+|    |__\  ___/\  ___/\  \___|   Y  \     \___|   Y  \  ___/\  \___|    <             
+|_______ \___  >\___  >\___  >___|  /\______  /___|  /\___  >\___  >__|_ \            
+        \/   \/     \/     \/     \/        \/     \/     \/     \/     \/            
+__________                  __           ___ ___      ___.                       __   
+\______   \_____    _______/  |_  ____  /   |   \ __ _\_ |__        ____   _____/  |_ 
+ |     ___/\__  \  /  ___/\   __\/ __ \/    ~    \  |  \ __ \      /    \_/ __ \   __\
+ |    |     / __ \_\___ \  |  | \  ___/\    Y    /  |  / \_\ \    |   |  \  ___/|  |  
+ |____|    (____  /____  > |__|  \___  >\___|_  /|____/|___  / /\ |___|  /\___  >__|  
+                \/     \/            \/       \/           \/  \/      \/     \/      
+
+""", 'green')
+    print("Welcome to LeechCheck!\n[1] Leech Recent Combos\n[2] Search Combos for Specific Keyword")
+    s = int(input("> "))
+    if s == 1:
+        main()
+    elif s == 2:
+        search()
+    else:
+        pass
+menu()
 proxy()
